8517d85e93dd4755bac99cb89f218355b4acb77b
dysha <dyha035@gmail.com>
2022-03-22 08:34:41 +0500

    version 1

---
+++ 18_prakt/mih/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace mih
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        bool IsServActive = false;
+        //прослушиваемый порт
+        int port = 8888;
+        //объект, прослушивающий порт
+        TcpListener listener;
+        Thread listenThread;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_Closed;
+        }
+
+        private void Start_Serv(object sender, RoutedEventArgs e)
+        {
+            IsServActive = true;
+            startServ = sender as Button;
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            listener.Start();
+            listenThread = new Thread(() => listen());
+            listenThread.Start();
+            startServ.IsEnabled = false;
+            stopServ.IsEnabled = true;
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            StoppingServer();
+        }
+
+        //обработка сообщений от клиента
+        public void Process(TcpClient tcpClient)
+        {
+            TcpClient client = tcpClient;
+            NetworkStream stream = null; //получение канала связи с клиентом
+
+            try //означает что в случае возникновении ошибки, управление перейдёт к блоку catch
+            {
+                //получение потока для обмена сообщениями
+                stream = client.GetStream(); //получение канала связи с клиентом
+                                             // буфер для получаемых данных
+                byte[] data = new byte[64];
+                //цикл ожидания и отправки сообщений
+                while (true)
+                {
+                    //==========================получение сообщения============================
+                    //объект, для формирования строк
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    //до тех пор, пока в потоке есть данные
+                    do
+                    {
+                    //из потока считываются 64 байта и записываются в data начиная с 0
+                    bytes = stream.Read(data, 0, data.Length);
+                        //из считанных данных формируется строка
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+                    if (IsServActive==false)
+                    {
+                        return;
+                    }
+                    //преобразование сообщения
+                    string message = builder.ToString();
+                    //вывод сообщения в лог сервера
+                    Dispatcher.BeginInvoke(new Action(() => log.Items.Add(message)));
+                    string[] mes = message.Split(':');
+                    char[] chars = mes[1].ToCharArray();
+                    Array.Reverse(chars);
+                    mes[1]=new string(chars);
+                    //==========================отправка сообщения=============================
+                    //преобразование сообщения в набор байт
+                    data = Encoding.Unicode.GetBytes(String.Join(":",mes));
+                    //отправка сообщения обратно клиенту
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex) //если возникла ошибка, вывести сообщение об ошибке
+            {
+                Dispatcher.BeginInvoke(new Action(() => log.Items.Add(ex.Message)));
+            }
+            finally //после выхода из бесконечного цикла
+            {
+                //освобождение ресурсов при завершении сеанса
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+        //функция ожидания и приёма запросов на подключение
+        void listen()
+        {
+            //цикл подключения клиентов
+            while (true)
+            {
+                try
+                {
+                    //принятие запроса на подключение
+                    TcpClient client = listener.AcceptTcpClient();
+                    string s="client connected to " + IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()) + " on port number " + ((IPEndPoint)client.Client.RemoteEndPoint).Port.ToString();
+                    Dispatcher.BeginInvoke(new Action(() => log.Items.Add(s)));
+                    //создание нового потока для обслуживания нового клиента
+                    Thread clientThread = new Thread(() => Process(client));
+                    clientThread.Start();
+                } catch  { break; }
+            }
+        }
+
+        private void Stop_Serv(object sender, RoutedEventArgs e)
+        {
+            StoppingServer();
+            stopServ.IsEnabled = false;
+            startServ.IsEnabled = true;
+        }
+        private void StoppingServer()
+        {
+            IsServActive = false;
+            listenThread?.Interrupt();
+            listener?.Stop();
+        }
+    }
+}
---
+++ 18_prakt_2/mih/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace mih
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int port = 8888;
+        //ip адрес сервера
+        string address = "127.0.0.1";
+        //объявление TCP клиента
+        TcpClient client = null;
+        //объявление канала соединения с сервером
+        NetworkStream stream = null;
+        //имя пользователя
+        string userName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            DisconnectClient();
+        }
+
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            //получение имени пользователя
+            userName = name.Text;
+            try //если возникнет ошибка - переход в catch
+            {
+                //создание клиента
+                client = new TcpClient(address, port);
+                //получение канала для обмена сообщениями
+                stream = client.GetStream();
+                Thread listenThread = new Thread(() => listen());
+                listenThread.Start();
+                ConnectButton.IsEnabled = false;
+                DisconnectButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                log.Items.Add(ex.Message);
+            }
+        }
+        void listen()
+        {
+            try //в случае возникновения ошибки - переход к catch
+            {
+                //цикл ожидания сообщениями
+                while (true)
+                {
+                    //буфер для получаемых данных
+                    byte[] data = new byte[64];
+                    //объект для построения смтрок
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    //до тех пор, пока есть данные в потоке
+                    do
+                    {
+                        //получение 64 байт
+                        bytes = stream.Read(data, 0, data.Length);
+                        //формирование строки
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+                    if (bytes == 0)
+                    {
+                        MessageBox.Show("Сервер разорвал подключение");
+                        Dispatcher.BeginInvoke(new Action(() => DisconnectClient()));
+                        return;
+                    }
+                    //получить строку
+                    string message = builder.ToString();
+                    //вывод сообщения в лог клиента
+                    Dispatcher.BeginInvoke(new Action(() => log.Items.Add("Сервер: " + message)));
+                }
+            }
+            catch (Exception ex)
+            {
+                //вывести сообщение об ошибке
+                MessageBox.Show(ex.Message);
+                Dispatcher.BeginInvoke(new Action(() => DisconnectClient()));
+            }
+        }
+
+        private void Send(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (stream != null)
+                {
+                    //получение сообщения
+                    string message = msg.Text;
+                    //добавление имени пользователя к сообщению
+                    message = String.Format("{0}: {1}", userName, message);
+                    //преобразование сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    //отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                } else
+                {
+                    MessageBox.Show("Вы не подключены к серверу");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                DisconnectClient();
+            }
+            
+        }
+
+        private void Disconnect(object sender, RoutedEventArgs e)
+        {
+            DisconnectClient();
+        }
+        private void DisconnectClient()
+        {
+            ConnectButton.IsEnabled = true;
+            DisconnectButton.IsEnabled = false;
+            stream?.Close();
+            stream = null;
+            client?.Close();
+            client = null;
+        }
+    }
+}
